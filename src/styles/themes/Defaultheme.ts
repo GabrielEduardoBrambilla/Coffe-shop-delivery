4ab169705478b02fa613b9170ce457aa72c0cdc6
Brambs <gabriel128.br@gmail.com>
2024-04-02 20:49:44 -0300

    DeafaultTheme added

---
+++ src/styles/themes/Defaultheme.ts
@@ -0,0 +1,17 @@
+export const defaultTheme = {
+  'yellow-100': '#F1E9C9',
+  'yellow-300': '#DBAC2C',
+  'purple-100': '#EBE5F9',
+  'purple-300': '#8047F8',
+  'purple-700': '#4B2995',
+  title: '#272221',
+  subtitle: '#403937',
+  text: '#574F4D',
+  label: '#8D8686',
+  hover: '#D7D5D5',
+  button: '#E6E5E5',
+  input: '#EDEDED',
+  card: '#F3F2F2',
+  background: '#FAFAFA',
+  white: '#FFFFFF'
+}
