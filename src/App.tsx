eff0fab7900b1678052ed0939a4ea60b2e11e0e3
Brambs <gabriel128.br@gmail.com>
2024-04-02 21:27:12 -0300

    Clean-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/Defaultheme'
 import { GlobalStyle } from './styles/global'
