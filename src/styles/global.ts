3957c0b6bd040d4ca077a5ff2d5eb0c8669ef798
Brambs <gabriel128.br@gmail.com>
2024-04-05 11:10:20 -0300

    Phosphor added and cleanup

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -15,7 +15,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body, input, textarea, button {
-    font-family: 'Roboto', sans-serif;
+    font-family: 'Baloo', sans-serif;
     font-weight: 400;
     font-size: 1rem;
   }
